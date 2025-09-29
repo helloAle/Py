a20a8e2bb53a3aec2f836c89fbb3c7c62ae07406
Alexandre <89224363+helloAle@users.noreply.github.com>
2025-09-29 13:31:22 -0300

    Create snake.py

---
+++ snake.py
@@ -0,0 +1,56 @@
+from tkinter import *
+import random
+
+Game_width = 600
+Game_height = 600
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#12121A"
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
+
+window = Tk()
+window.title("Snake Game")
+window.resizable(False, False)
+
+score = 0
+direction = 'down'
+
+label = Label(window, text="Score:{}".format(score), font=('consolas', 30))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=Game_height, width=Game_width)
+canvas.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+window.mainloop()
