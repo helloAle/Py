b2ade65cc9dce3a04360432d1543ecf308773ab5
Alexandre <89224363+helloAle@users.noreply.github.com>
2026-03-05 16:23:48 -0300

    Add repetition examples in Python
    Add example code demonstrating repetition in Python using for and while loops.

---
+++ py2026/(2.13)_repetition_in_python.py
@@ -0,0 +1,14 @@
+# ======================= 13 -> repetições ============================
+
+# for i in range(1,11):
+#   print(i)
+# i = int(input('Digite um numero: '))
+
+# while i >= 0:
+#   print(i, i==0)
+#   i = i-1
+
+# senha = ''
+# while senha != '123':
+#   senha = input('Qual é a senha do sistema?   ')
+# print('Login :)')
