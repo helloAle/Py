4e6ea0ca1ccf11153af10dc1448ffede34bb0534
Alexandre <89224363+helloAle@users.noreply.github.com>
2026-03-05 14:10:59 -0300

    Add age verification script
    Implement age verification logic using input and logical operators.

---
+++ py2026/1_age_verification.py
@@ -0,0 +1,15 @@
+# igual ou maior que 18 anos
+
+idade = int(input('Qual sua idade?    '))
+verificador = idade >= 18
+# print(verificador)
+
+# OPERDORES LOGICOS
+# and or not
+carteira = True
+verificador = idade >= 18 and carteira
+print(verificador)
+
+# cnh = input('Possui carteira de motorista? | sim - 0 | não - 1 |    ')
+# verificador = idade >= 18 and (cnh == '0' or cnh == 'sim')
+# print(verificador)
