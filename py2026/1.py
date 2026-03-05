2ea89e53c430b47fec02ee985a491c6e35452c02
Alexandre <89224363+helloAle@users.noreply.github.com>
2026-03-05 08:15:07 -0300

    Refactor variable 'mensagem' to 'altura'

---
+++ py2026/1.py
@@ -0,0 +1,16 @@
+#=========================seção 1=============================
+
+meu_nome = 'Alexandre'  #string
+idade = 24              #Integer
+altura = 1.83         #string
+engenheiro = True       #boolean
+
+
+print('meu_nome')
+print(type(meu_nome))
+print('idade')
+print(type(idade))
+print('altura')
+print(type(altura))
+print(type(engenheiro))
+
