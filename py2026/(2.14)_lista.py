b2dac62490708327c63047500f3df5d8dc26ad2b
Alexandre <89224363+helloAle@users.noreply.github.com>
2026-03-06 15:05:30 -0300

    Create example code for lists and tasks
    Add examples of lists, dictionaries, and tuples in Python.

---
+++ py2026/(2.14)_lista.py
@@ -0,0 +1,44 @@
+# ==================== 14  -> listas; dicionarios e Tuplas =================
+# quando eu utilizo uma lista?      quando utilizo  varios itens mutaveis
+# quando eu utilizo tuplas?         quando utilizo itens imutaveis
+#  quando eu utilizo dicionarios?   quando
+
+
+# frutas = ['abacate', 'banana', 'morango']
+
+# print(frutas)
+
+# i = input("nova fruta:  ")
+# frutas.append(i)
+# print(len(frutas))
+# print(frutas)
+
+# i = input('remover fruta: ')
+# frutas.remove(i)
+# print(len(frutas))
+# print(frutas)
+
+# --------------------------------------------
+# i = 1
+# tarefas = []
+
+# while i <= 5:
+#   ta = input('adicionar tarefa: ')
+#   tarefas.append(ta)
+#   i = i+1
+# # print(tarefas)
+# for tarefa in tarefas:
+#   print(f'tarefas: {tarefa}')
+
+# --------------------------------------------
+
+# tarefas = []
+
+# tarefas.append('coisa 1')
+# tarefas.append('coisa 2')
+# tarefas.append('coisa 3')
+# tarefas.append('coisa 4')
+
+# print('Minhas tarefas:  ')
+# for tarefa in tarefas:
+#   print(f'tarefa: {tarefa}')
