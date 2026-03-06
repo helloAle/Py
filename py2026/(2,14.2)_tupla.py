f7406dd0acaf0848925371ff013ae9d9f1ba0862
Alexandre <89224363+helloAle@users.noreply.github.com>
2026-03-06 15:06:05 -0300

    Create tuple of month names and print February
    Add a tuple containing month names and print the second month.

---
+++ py2026/(2,14.2)_tupla.py
@@ -0,0 +1,9 @@
+#                 14.2 -> tupla
+
+# algo que nao muda, por exemplo: meses, dias da semana e etc
+
+meses = ('janeiro', 'fevereiro', 'marco')
+
+print(meses[1])
+
+# meses.append('abril') não vai funcionar
