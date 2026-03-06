31a9e7100e0c3baec72e2a2b81a4a3647b2b1f08
Alexandre <89224363+helloAle@users.noreply.github.com>
2026-03-06 15:16:47 -0300

    Refactor user dictionary and add student input

--- py2026/(2.14.3)_dicionario.py
+++ py2026/(2.14.3)_dicionario.py
@@ -1,11 +1,20 @@
 #               14.3 -> dicionario
 
-usuario = {
-    'nome': 'ale',
-    'idade': 24,
-    'departamento': 'TI'
+# usuario = {
+#     'nome': 'ale',
+#     'idade': 24,
+#     'departamento': 'TI'
+# }
+
+# usuario['idade'] = 25
+
+# print(usuario)
+
+
+aluno = {
+    'nome': input('Nome do aluno: '),
+    'idade': input('Idade do aluno: '),
+    'nota': input('Nota do aluno: ')
 }
 
-usuario['idade'] = 25
-
-print(usuario)
+print(f'{aluno['nome']} tem {aluno['idade']} anos de idade e tirou {aluno['nota']} na média')
