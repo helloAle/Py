47c96e6eb3d72e861b9b08692f43c1b7a47fc8b2
Alexandre <89224363+helloAle@users.noreply.github.com>
2026-03-10 10:12:18 -0300

    Add Carro class with methods for control and info

---
+++ secao_3/class_Carro.py
@@ -0,0 +1,30 @@
+class Carro:
+  def __init__(self, cor, ano):
+    self.cor = cor
+    self.ano = ano
+    self.ligado = False
+
+  def informacoes(self):
+    print(f'A cor do carro e {self.cor}')
+    print(f'O ano do carro e {self.ano}')
+
+  def ligar(self):
+    if not self.ligado:
+      self.ligado = True
+      print('O carro foi ligado')
+    else:
+      print('O carro ja estava ligado')
+
+  def desligar(self):
+    if self.ligado:
+      self.ligado = False
+      print('O carro foi desligado')
+    else:
+      print('O carro estava desligado')
+
+carro1 = Carro('Preto', 2021)
+carro1.informacoes()
+carro1.ligar()
+carro1.desligar()
+
+# ===========================================
