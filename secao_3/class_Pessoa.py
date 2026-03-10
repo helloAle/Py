fc185df553a122d77020b4f54f16354c4521e94e
Alexandre <89224363+helloAle@users.noreply.github.com>
2026-03-10 10:35:44 -0300

    Implement age update method in class Pessoa
    Add method to update age with validation

--- secao_3/class_Pessoa.py
+++ secao_3/class_Pessoa.py
@@ -12,8 +12,15 @@ class Pessoa:
   def promover(self, novo_cargo):
     print(f'{self.nome} foi poromovido(a) para a nova funcao de {novo_cargo}!')
 
+  def atualizar_idade(self, nova_idade):
+    if nova_idade > self.idade:
+      print(f'Atualizando a Idade de {self.idade} para {nova_idade}')
+    else:
+      print(f'A nova idade deve ser maior que a anterior')
+
 colaborador1 = Pessoa('Ana', 36, 'Assistente Jr')
 colaborador2 = Pessoa('Joao', 45, 'Dev Jr')
 
 colaborador1.informacoes()
 colaborador1.promover('Assistente Pleno')
+colaborador1.atualizar_idade(37)
