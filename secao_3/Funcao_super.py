67efa8c708e3c5a4d2903bba92aceb6b5c7d5a76
Alexandre <89224363+helloAle@users.noreply.github.com>
2026-03-19 15:16:27 -0300

    Create Funcao_super.py

---
+++ secao_3/Funcao_super.py
@@ -0,0 +1,34 @@
+# sobre a função super()
+
+# SISTEMA DE ESCOLA
+# preciso criar classes para diferentes cargos, pessoas
+
+class Escola():
+  def __init__(self, nome, idade):
+    self.nome = nome
+    self.idade = idade
+
+# classes filhas
+class Aluno(Escola):
+  def __init__(self, nome, idade, ano):
+    super().__init__(nome, idade)
+    self.ano = ano
+
+  
+class Professor(Escola):
+  def __init__(self, nome, idade, materia):
+    super().__init__(nome, idade)
+    self.materia = materia
+
+
+class Assistente(Escola):
+  def __init__(self, nome, idade, bloco):
+    super().__init__(nome, idade)
+    self.bloco = bloco
+    
+a1 = Aluno('Marcos', 12, 8)
+p1 = Professor('Roberto', 34, 'Geometria')
+as1 = Assistente('Ana Maria', 29, 'C')
+
+print(a1.ano)
+
