ffe29e26436af2969bf80ed15fd52daaf17bb5c9
Alexandre <89224363+helloAle@users.noreply.github.com>
2026-03-23 16:19:13 -0300

    Update and rename 27_01_nested_class.py to 27_nested_class.py

---
+++ secao_3/27_nested_class.py
@@ -0,0 +1,40 @@
+# criar a classe main
+
+class Computador:
+  def __init__(self, modelo, gpu_nome, gpu_memoria, cpu_nome, cpu_cores, cpu_clock):
+    self.modelo = modelo
+    self.gpu_nome = gpu_nome
+    self.gpu = self.GPU(gpu_nome, gpu_memoria) # bem aqui ocorre a interação (self.gpu chama self.GPU)
+    self.cpu = self.CPU(cpu_nome, cpu_cores, cpu_clock)
+
+  
+  def mostrar_configuracao(self):
+    print(f'Computador: {self.modelo}')
+    self.gpu.mostrar_gpu()
+    self.cpu.mostrar_cpu()
+
+  class GPU: # NESTED CLASS
+    def __init__(self, nome, memoria_gb):
+      self.nome = nome
+      self.memoria_gb = memoria_gb
+
+    def mostrar_gpu(self):
+      print(f'GPU: {self.nome} {self.memoria_gb}')
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  class CPU:
+    def __init__(self, nome, cores, clock_ghz):
+      self.nome = nome
+      self.cores = cores
+      self.clock_ghz = clock_ghz
+
+    def mostrar_cpu(self):
+      print(f'CPU: {self.nome} - {self.cores} Núcleos - {self.clock_ghz} gHz ')
+    
+
+#  UTILIZAÇÃO
+
+
+pc1 = Computador('Dell XPS', 'NVIDIA RTX 4090', 24, 'Intel core I7', 8, 4.6)
+pc1.mostrar_configuracao()
