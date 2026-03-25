1e64acad35b7024ac1ef46387d97eab7400f4cb2
Alexandre <89224363+helloAle@users.noreply.github.com>
2026-03-25 10:35:23 -0300

    Update janela.py

--- solo/janela.py
+++ solo/janela.py
@@ -1,25 +1,54 @@
 import tkinter as tk
 
+contas = [
+    ('lan', '123'),
+    ('aleale', 'ale123')
+]
+
 # nome = input('Digite seu nome: ')
 
 janela = tk.Tk()
-janela.title('janela nome, esse e o nome da janela :D')
-janela.geometry('500x500')
+janela.title('Login')
+janela.geometry('400x300')
 
-texto = tk.Label(janela, text=f"INSIRA SEU NOME")
-texto.pack()
+tk.Label(janela, text='user').pack()
+usuario_entry = tk.Entry(janela)
+usuario_entry.pack()
 
-def resposta_botao():
-    nome = entrada_texto.get()
-    texto['text'] = f'o {nome} apertou aqui ow'
+tk.Label(janela, text='password').pack()
+senha_entry = tk.Entry(janela)
+senha_entry.pack()
 
-entrada_texto = tk.Entry(janela)
-entrada_texto.pack()
-tk.Button(janela, text="botaoao", command=resposta_botao).pack()
+def login():
+    usuario = usuario_entry.get()
+    senha = senha_entry.get()
+    if (usuario, senha) in contas:
+        mensagem_label['text'] = "logado"
+    else:
+        mensagem_label['text'] = 'credenciais invalidas'
+
+tk.Button(janela, text='login', command=login).pack()
+
+mensagem_label = tk.Label(janela, text='')
+mensagem_label.pack()
+
+
+
+# texto = tk.Label(janela, text=f"INSIRA SEU NOME")
+# texto.pack()
+
+# def resposta_botao():
+#     nome = entrada_texto.get()
+#     texto['text'] = f'o {nome} apertou aqui ow'
+
+# entrada_texto = tk.Entry(janela)
+# entrada_texto.pack()
+# tk.Button(janela, text="botaoao", command=resposta_botao).pack()
 
 # class Application:
 #     def __init__(self, master=None):
 #         pass
 
 # Application(janela)
+
 janela.mainloop()
