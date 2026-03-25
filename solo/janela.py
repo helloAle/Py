c03ff13e9731f6fedb6ef04c2552ecb3f6c5dffb
Alexandre <89224363+helloAle@users.noreply.github.com>
2026-03-25 10:22:56 -0300

    Update janela.py

--- solo/janela.py
+++ solo/janela.py
@@ -1,12 +1,21 @@
 import tkinter as tk
 
-nome = input('Digite seu nome: ')
+# nome = input('Digite seu nome: ')
 
 janela = tk.Tk()
 janela.title('janela nome, esse e o nome da janela :D')
 janela.geometry('500x500')
 
-tk.Label(janela, text=f"oi, {nome} :D").pack()
+texto = tk.Label(janela, text=f"INSIRA SEU NOME")
+texto.pack()
+
+def resposta_botao():
+    nome = entrada_texto.get()
+    texto['text'] = f'o {nome} apertou aqui ow'
+
+entrada_texto = tk.Entry(janela)
+entrada_texto.pack()
+tk.Button(janela, text="botaoao", command=resposta_botao).pack()
 
 # class Application:
 #     def __init__(self, master=None):
