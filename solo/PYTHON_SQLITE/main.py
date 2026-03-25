9d5657a2692006962150f7461832b0b91301a2e6
Alexandre <89224363+helloAle@users.noreply.github.com>
2026-03-25 14:06:38 -0300

    Update main.py

--- solo/PYTHON_SQLITE/main.py
+++ solo/PYTHON_SQLITE/main.py
@@ -16,13 +16,20 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS contas_bancarias(
 #                (titular, saldo, cpf) VALUES
 #                ('Ale', -7000, '123456789')""")
 
-# cursor.execute("""SELECT titular, saldo FROM contas_bancarias""")
-cursor.execute("""SELECT titular, saldo FROM contas_bancarias
-               WHERE saldo < 0""")
+# cursor.execute("""SELECT titular, saldo FROM contas_bancarias
+#                WHERE saldo < 0""")
+# cursor.execute("DELETE FROM contas_bancarias WHERE id = 1")
+
+cursor.execute("""UPDATE contas_bancarias
+               SET saldo = -5000
+               WHERE id = 2""")
+cursor.execute("""SELECT titular, saldo FROM contas_bancarias""")
 contas = cursor.fetchall()
+
 for conta in contas:
     titular, saldo = conta
     print(f"""titular: {titular}
-saldo: {saldo}\n""")
+saldo: {saldo}""")
+    print('\n')
 
 conexao.commit()
