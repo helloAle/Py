54191ce981f3c97b1abd3cb03e60d02d5d7ef2fe
Alexandre <89224363+helloAle@users.noreply.github.com>
2026-03-25 10:55:13 -0300

    fix: corrige uso de Frame e troca de páginas no Tkinter
    Corrige a criação do Frame pagina_login, removendo o uso incorreto de .pack() na atribuição,
    garantindo que o objeto seja armazenado corretamente.
    
    Ajusta também a criação do pagina_sistema, vinculando-o à janela principal.
    
    Com isso, a navegação entre telas (login → sistema) via pack_forget() e pack() passa a funcionar corretamente.

--- solo/janela.py
+++ solo/janela.py
@@ -2,16 +2,18 @@ import tkinter as tk
 
 contas = [
     ('lan', '123'),
-    ('aleale', 'ale123')
+    ('ale', 'ale')
 ]
 
-# nome = input('Digite seu nome: ')
 
 janela = tk.Tk()
 janela.title('Login')
 janela.geometry('400x300')
 
-pagina_login = tk.Frame(janela).pack()
+# LOGIN
+
+pagina_login = tk.Frame(janela)
+pagina_login.pack()
 
 tk.Label(pagina_login, text='user').pack()
 usuario_entry = tk.Entry(pagina_login)
@@ -21,11 +23,14 @@ tk.Label(pagina_login, text='password').pack()
 senha_entry = tk.Entry(pagina_login)
 senha_entry.pack()
 
+# FUNÇÃO DE LOGIN
+
 def login():
     usuario = usuario_entry.get()
     senha = senha_entry.get()
     if (usuario, senha) in contas:
-        mensagem_label['text'] = "logado"
+        pagina_login.pack_forget()
+        pagina_sistema.pack()
     else:
         mensagem_label['text'] = 'credenciais invalidas'
 
@@ -34,6 +39,12 @@ tk.Button(pagina_login, text='login', command=login).pack()
 mensagem_label = tk.Label(pagina_login, text='')
 mensagem_label.pack()
 
+# SISTEMA
+
+pagina_sistema = tk.Frame()
+tk.Label(pagina_sistema, text='Bienvenido!').pack()
+
+# JANELA
 
 janela.mainloop()
 
